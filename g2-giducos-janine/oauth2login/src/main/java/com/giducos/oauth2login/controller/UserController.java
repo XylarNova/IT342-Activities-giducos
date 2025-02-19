97dfe37d0b087b3b1894f6d8d53a575eccd180e2
LAB13_USER ON WS12 <L13X13W12@citulab.net>
2025-02-19 14:39:59 +0800

    add all

--- g2-giducos-janine/oauth2login/src/main/java/com/giducos/oauth2login/controller/UserController.java
+++ g2-giducos-janine/oauth2login/src/main/java/com/giducos/oauth2login/controller/UserController.java
@@ -1,2 +1,27 @@
-package com.giducos.oauth2login.controller;public class UserController {
+package com.giducos.oauth2login.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+@RestController
+public class UserController {
+
+    @GetMapping
+    public String index(){
+        return "<h1> COLD LABORATORY</h1>";
+    }
+
+    @GetMapping("/user-info")
+    public Map<String, Object> getUserprofile(@AuthenticationPrincipal OAuth2User oAuth2User){
+        return oAuth2User.getAttributes();
+    }
+    @GetMapping("/secured")
+    public String securedEndpoint(){
+        return "<h1> secured endpoint</h1>";
+
+    }
+
 }
